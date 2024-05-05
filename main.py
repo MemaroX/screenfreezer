d3812b6a94facc6702914ce50d1f3b4f0913e51a
Maher Gomaa Ismaeel Sayed <116512721+MemaroX@users.noreply.github.com>
2024-05-05 21:19:28 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import keyboard  # Import keyboard library for detecting keypress events  
+import subprocess  # Import subprocess module for running system commands  
+while True:
+  sleep(1)
+  if keyboard.is_pressed('x'):
+    break
+    # Break out of infinite loop when 'X' is pressed
+print("PC has been stunned.")
+process = subprocess.Popen(["systemctl", "stop", "lightdm"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+output, error = process.communicate()
+if error != b"":
+  print(error)
+else:
+  print("System stopped successfully.")
+  #In this example, we use `systemctl` to stop the Light Display Manager (LDM), which is responsible for managing graphical sessions on Linux systems like Ubuntu and Fedora. By stopping LDM, we effectively freeze the system until it is restarted or shut down manually.
